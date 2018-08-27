d3f282f991cebc2432b95aeb867d365be80f211e
Stephen Brown <stephen@obashitech.com>
2018-08-27 16:25:20 +0100

    setup basic tooling

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
-all:
+verify:
 	tidy -mqi -config tidy-config index.html
+	node_modules/.bin/eslint checklist.js --fix
 
--- checklist.js
+++ checklist.js
@@ -1,16 +1,16 @@
-function isFinished() {
-    var checkboxes = document.getElementsByClassName("form-check-input");
-    var allFinished = true;
-    for (var i = 0; i < checkboxes.length; i++) {
-        if (!checkboxes[i].checked) {
-            allFinished = false;
-            break;
-        }
+function isFinished () { // eslint-disable-line no-unused-vars
+  var checkboxes = document.getElementsByClassName('form-check-input')
+  var allFinished = true
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (!checkboxes[i].checked) {
+      allFinished = false
+      break
     }
+  }
 
-    if (allFinished) {
-        document.getElementById("checklistFinished").innerHTML = "Congratulations!";
-    } else {
-        document.getElementById("checklistFinished").innerHTML = "Finish your checklist!";
-    }
+  if (allFinished) {
+    document.getElementById('checklistFinished').innerHTML = 'Congratulations!'
+  } else {
+    document.getElementById('checklistFinished').innerHTML = 'Finish your checklist!'
+  }
 }
