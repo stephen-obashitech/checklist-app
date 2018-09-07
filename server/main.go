56c7ad60553360668a17d3004fbd646b6eafa547
Stephen Brown <stephen@obashitech.com>
2018-09-07 13:50:53 +0100

    Implement basic saving on server end

--- server/main.go
+++ server/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"os"
+	"io/ioutil"
 )
 
 type Checkboxes struct {
@@ -34,10 +36,35 @@ func HandleFinished(c *gin.Context) {
 	c.JSON(200, response)
 }
 
+func HandleSave(c *gin.Context) {
+	body,err  := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	saveToFile(string(body))
+}
+
+func saveToFile(checklistState string) {
+	f, err := os.Create("checklist.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(checklistState)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
 	r.POST("/finished", HandleFinished)
+	r.POST("/save", HandleSave)
+	// TODO save file handler
 
 	fmt.Println("***Server Started!***")
 
